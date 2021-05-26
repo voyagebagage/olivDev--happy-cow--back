8c7270a4859ba71b62656e08a21a78c401dbe06a
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-26 22:43:14 +0700

    w/ filtre oneword

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const restaurants = require("./restaurants.json");
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(formidable());
 
 //////_C.O.N.F.I.G_\\\\\\
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb://localhost:27017/happyCow", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   //   useFindAndModify: false,
@@ -20,9 +21,45 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.get("/", (req, res) => {
   res.json("welCom to my happyCow");
 });
+
 app.get("/restaurants", (req, res) => {
-  res.status(200).json(restaurants);
+  try {
+    const limit = Number(req.query.limit) || 10;
+    const skip = Number(req.query.skip) || 0;
+    const { name, description, type } = req.query;
+    let filters;
+
+    if (name || description || type) {
+      filters = new RegExp(name || description || type, "i");
+      // filters.description = new RegExp(description, "i");
+      // filters.type = new RegExp(type, "i");
+    }
+    // console.log(filters);
+    for (let i = 0; i < restaurants.length; i++) {
+      const element = restaurants[i].name;
+      if (filters.test(element)) {
+        console.log(element);
+      }
+
+      // element.push(
+      //   restaurants[i].match(
+      //     filters.name || filters.description || filters.type
+      //   )
+
+      // const result = restaurants.filter((restaurant) => restaurant === filters);
+      // console.log(result);
+    }
+    // const result = restaurants.filter((restaurant) => restaurant === filters);
+    // console.log(element);
+    const searchResult = restaurants.slice(skip, limit);
+    // console.log(restaurants);
+    // console.log(searchResult);
+    res.status(200).json(searchResult);
+  } catch (error) {
+    console.log(error);
+  }
 });
+
 app.all("*", (req, res) => {
   res.status(404).json({ error: "None existing route" });
 });
