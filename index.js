335edacc75bcffbd6b07a90503ead246d946682f
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-28 12:27:54 +0700

    added search of many words and rating sort

--- index.js
+++ index.js
@@ -30,62 +30,62 @@ app.get("/restaurants", (req, res) => {
     let filters;
     const result = [];
 
+    //IF query then without case, we split in case of many words<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
     if (name || description || type) {
       filters = new RegExp(name || description || type, "i");
+
+      filters.toString().split(" ");
 
       for (let i = 0; i < restaurants.length; i++) {
         let result1;
         let result2;
         let tempElem = [];
+        //TARGETING description Key<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
         if (restaurants[i].description) {
           tempElem.push(restaurants[i].description.split(" "));
-          // console.log("tempELM===>>>" + tempElem);
           for (let k = 0; k < tempElem.length; k++) {
             const elementDescr = tempElem[k];
+            //TESTING description Key WORDS one by one<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
             if (filters.test(elementDescr)) {
-              // console.log("elementDescr---------------------------");
-              // if (restaurants[i].name !== restaurants[i - 1].name) {
+              //IF Match<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
               if (restaurants[i].name) {
                 result1 = restaurants[i];
               }
-
-              // console.log("result DESC ===>" + restaurants[i].name);
               break;
-              // }
             }
           }
         }
         const element1 = restaurants[i].name;
         const element3 = restaurants[i].type;
+        //TARGETING name & type Key<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
         if (filters.test(element1) || filters.test(element3)) {
           if (restaurants[i].name) {
             result2 = restaurants[i];
           }
-          // console.log(
-          //   "result NAME & TYPE ================================>" +
-          //     restaurants[i].name
-          // );
         }
+        //COLLECTING in one ARRAY<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
         if (result1 || result2) {
           if (result1 === result2) {
             result.push(result1 || result2);
-            // console.log("result " + result);
           } else if (result1) {
-            // console.log("result11111" + result1);
             result.push(result1);
           } else if (result2) {
-            // console.log("result22222" + result2);
             result.push(result2);
           }
         }
       }
+      //SORTING by rating-[NO QUERY]<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
+      result.sort((a, b) => (a.rating < b.rating ? 1 : -1));
+      // for (let j = 0; j < result.length; j++) {
+      //   console.log(result[j].rating);
+      // }
+      //LIMITTING the OUTPUT<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
       const searchResult = result.slice(skip, limit);
-      console.log(searchResult);
       res.status(200).json(searchResult);
     } else {
+      //sent by default<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
       const homeScreenResult = restaurants.slice(skip, limit);
-      // console.log(restaurants);
-      // console.log(searchResult);
+
       res.status(200).json(homeScreenResult);
     }
   } catch (error) {
